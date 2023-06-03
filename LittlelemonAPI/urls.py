dbf4210a7c8b2effcbbb69d061f00ae7abd18470
Doheera-kosi <kupourevans99@gmail.com>
2023-06-03 23:52:34 +0000

    Modify views.py, urls.py and serializers.py

--- LittlelemonAPI/urls.py
+++ LittlelemonAPI/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('menu-items', views.MenuItemsView.as_view()),
-  path('menu-items/<int:pk>', views.SingleMenuItemsView.as_view()),
+    path('menu-items/', views.menu_items, name='menu_items'),
+    path('menu-items/<int:id>/', views.single_item, name='single_item'),
 ]
--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import MenuItem
+from rest_framework import generics, status
 from .serializers import MenuItemSerializer
+
 # Create your views here.
+@api_view()
+def menu_items(request):
+    items = MenuItem.objects.all()
+    serialized_item = MenuItemSerializer(items, many=True)
+    return Response(serialized_item.data)
 
-class MenuItemsView(generics.ListCreateAPIView):
-  queryset = MenuItem.objects.all()
-  serializer_class = MenuItemSerializer
+@api_view(['GET'])
+def single_item(request, id):
+    try:
+        item = MenuItem.objects.get(id=id)
+        serialized_item = MenuItemSerializer(item)
+        return Response(serialized_item.data)
+    except MenuItem.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-class SingleMenuItemsView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
-  queryset = MenuItem.objects.all()
-  serializer_class = MenuItemSerializer
