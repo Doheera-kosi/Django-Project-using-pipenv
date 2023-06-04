71e9753e9c4d62df9d9dad4270614a3df321bb05
Doheera-kosi <kupourevans99@gmail.com>
2023-06-04 22:33:36 +0000

    Add searching & filtering techniques

--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -10,6 +10,18 @@ from .serializers import MenuItemSerializer
 def menu_items(request):
     if request.method == 'GET':
       items = MenuItem.objects.select_related('category').all()
+      category_name = request.query_params.get('category')
+      to_price = request.query_params.get('to_price')
+      search = request.query_params.get('search')
+      if category_name:
+        items = items.filter(category__title=category_name)
+        
+      if to_price:
+        items = items.filter(price__lte=to_price)
+      
+      if search:
+        items = items.filter(title__contains=search)
+        
       serialized_item = MenuItemSerializer(items, many=True)
       return Response(serialized_item.data)
 
