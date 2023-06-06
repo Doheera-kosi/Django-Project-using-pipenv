8cfd053b84a7f76c89bbab21ff46efdedf51d183
Doheera-kosi <kupourevans99@gmail.com>
2023-06-06 22:55:27 +0000

    Add secret endpoint path

--- LittlelemonAPI/urls.py
+++ LittlelemonAPI/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('menu-items/', views.menu_items, name='menu_items'),
     path('menu-items/<int:id>/', views.single_item, name='single_item'),
+    path('secret', views.secret)
 ]
