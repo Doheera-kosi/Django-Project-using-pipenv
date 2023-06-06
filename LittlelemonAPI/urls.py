e8a573417dac08d62ff51228ed6caf18e51425bb
Doheera-kosi <kupourevans99@gmail.com>
2023-06-06 23:40:54 +0000

    Modify urls

--- LittlelemonAPI/urls.py
+++ LittlelemonAPI/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('menu-items/',views.menu_items, name='menu_items'),
     path('menu-items/<int:id>/',views.single_item, name='single_item'),
-    path('secret/',views.secret)
+    path('secret/',views.secret),
+    path('api-token-auth/', obtain_auth_token)
 ]
